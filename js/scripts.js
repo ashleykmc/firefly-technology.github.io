1afeb81f39cafd26bab6096c97bf4fd8980cccd5
firefly-technology <74106277+firefly-technology@users.noreply.github.com>
2020-11-23 21:26:47 +0000

    slowed text change on header

--- js/scripts.js
+++ js/scripts.js
@@ -50,7 +50,7 @@
 		// An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
 		separator: ",",
 		// The delay between the changing of each phrase in milliseconds.
-		speed: 2000,
+		speed: 2500,
 		complete: function () {
 			// Called after the entrance animation is executed.
 		}
